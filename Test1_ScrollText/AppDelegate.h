45993a8047402ec87507d52d4a6da0b89408dafe
Dim <neetro@mail.ru>
2016-01-04 14:24:11 +0300

    Initial Commit

---
+++ Test1_ScrollText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test1_ScrollText
+//
+//  Created by Dim on 04.01.16.
+//  Copyright © 2016 TagsFor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test1_ScrollText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test1_ScrollText
+//
+//  Created by Dim on 04.01.16.
+//  Copyright © 2016 TagsFor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
